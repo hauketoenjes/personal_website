72b0cd2e5b2f2bb1f1e90b1f58fc7d823e7e3fd7
Hauke Tönjes <git@htoenjes.de>
2022-05-13 16:00:11 +0200

    Use Fontawesome icons

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,14 @@ import "../styles/globals.css";
 
 import type { AppProps } from "next/app";
 import { DefaultSeo } from "next-seo";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faTwitter,
+  faLinkedin,
+  faGithub,
+} from "@fortawesome/free-brands-svg-icons";
+
+library.add(faTwitter, faLinkedin, faGithub);
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
