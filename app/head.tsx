a2d52a3db686568d3068d0bb9a3fcbc202f9dc08
Hauke Tönjes <git@htoenjes.de>
2022-10-25 21:16:10 +0200

    feat: Add SEO description

--- app/head.tsx
+++ app/head.tsx
@@ -2,6 +2,10 @@ export default function Head() {
   return (
     <head>
       <title>Hauke Tönjes</title>
+      <meta
+        name="description"
+        content="Persönliche Internetseite von Hauke Tönjes. Informationen zu Projekten und Kontakt."
+      />
     </head>
   );
 }
