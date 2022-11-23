efab78ecfb0139fac17e02be8f35e68e3aa0b9cd
Hauke Tönjes <git@htoenjes.de>
2022-11-23 21:31:37 +0100

    fix: remove default SEO in layout - Default SEO in layout.tsx resulted in duplicate meta / title tags in the html

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,7 @@
 import { config } from "@fortawesome/fontawesome-svg-core";
 import "@fortawesome/fontawesome-svg-core/styles.css";
-import { NextSeo } from "next-seo";
 import Footer from "../components/footer";
 import Header from "../components/header";
-import { NEXT_SEO_DEFAULT } from "../next-seo.config";
 import "../styles/globals.css";
 
 config.autoAddCss = false;
@@ -18,7 +16,6 @@ export default function RootLayout({
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta charSet="utf-8" />
-        <NextSeo {...NEXT_SEO_DEFAULT} />
       </head>
       <body className="bg-neutral-900 text-neutral-100">
         <Header />
